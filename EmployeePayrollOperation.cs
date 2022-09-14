38b5c6daac5a36428e4362d36e5cd4bf26f3f204
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-14 13:40:00 +0530

    employee pay roll using multithreading uc 1

---
+++ EmployeePayrollOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    public class EmployeePayrollOperation
+    {
+        public List<EmployeeDetails> employeePayrollDetailsList = new List<EmployeeDetails>();
+        public void addEmployeeToPayroll(List<EmployeeDetails> employeePayrollDetailsList)
+        {
+            employeePayrollDetailsList.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee being added: " + employeeData.EmployeeName);
+                this.addEmployeeToPayroll(employeeData);
+                Console.WriteLine("Employee added: " + employeeData.EmployeeName);
+            });
+            Console.WriteLine(this.employeePayrollDetailsList.ToString());
+        }
+
+
+        public void addEmployeeToPayroll(EmployeeDetails emp)
+        {
+            employeePayrollDetailsList.Add(emp);
+        }
+
+    }
+}
